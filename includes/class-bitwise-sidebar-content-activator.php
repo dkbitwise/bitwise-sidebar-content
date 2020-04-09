7bcd707b152986c74dbe9e0f9a6a46ea893c59f5
dkbitwise <bit.tester.91@gmail.com>
2020-04-10 01:41:54 +0530

    Initial plugin files

---
+++ includes/class-bitwise-sidebar-content-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bitwiseacademy.com/
+ * @since      1.0.0
+ *
+ * @package    Bitwise_Sidebar_Content
+ * @subpackage Bitwise_Sidebar_Content/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Bitwise_Sidebar_Content
+ * @subpackage Bitwise_Sidebar_Content/includes
+ * @author     Bitwise <https://bitwiseacademy.com/>
+ */
+class Bitwise_Sidebar_Content_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-bitwise-sidebar-content-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bitwiseacademy.com/
+ * @since      1.0.0
+ *
+ * @package    Bitwise_Sidebar_Content
+ * @subpackage Bitwise_Sidebar_Content/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Bitwise_Sidebar_Content
+ * @subpackage Bitwise_Sidebar_Content/includes
+ * @author     Bitwise <https://bitwiseacademy.com/>
+ */
+class Bitwise_Sidebar_Content_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
